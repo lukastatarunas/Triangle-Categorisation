4985f58830c6eef2412d80869f72094155177301
lukastatarunas <lukasttatarunas@gmail.com>
2019-09-08 14:04:04 +0300

    Fixed the algorithm

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 getTriangleType = (x, y, z) => {
-    if (x === y && x === z) console.log(`The triangle is equilateral!`)
+    if (x + y < z || x + z < y || y + z < x) console.log(`The triangle does not exist!`)
+    else if (x === y && x === z) console.log(`The triangle is equilateral!`)
     else if (x === y || x === z || y === z) console.log(`The triangle is isosceles!`)
     else console.log(`The triangle is scalene!`)
 }
 
+getTriangleType(1, 10, 12)
 getTriangleType(3, 3, 3)
 getTriangleType(3, 3, 5)
 getTriangleType(3, 5, 7)
